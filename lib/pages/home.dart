1d7cde4446b39b831d81a2a53a7267dabcf6622d
afandy72 <youssefafandy142@gmail.com>
2023-03-30 13:42:29 +0200

    29

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -5,39 +5,41 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.green,
-        actions: [
-          Row(
-            children: [
-              Stack(
-                children: [
-                  Container(
-                    child: Text(
-                      "8",
-                      style: TextStyle(fontSize: 15, color: Colors.black),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.green,
+          actions: [
+            Row(
+              children: [
+                Stack(
+                  children: [
+                    Container(
+                      child: Text(
+                        "8",
+                        style: TextStyle(fontSize: 15, color: Colors.black),
+                      ),
+                      padding: EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                          color: Color.fromARGB(211, 164, 255, 193),
+                          shape: BoxShape.circle),
                     ),
-                    padding: EdgeInsets.all(5),
-                    decoration: BoxDecoration(
-                        color: Color.fromARGB(211, 164, 255, 193),
-                        shape: BoxShape.circle),
-                  ),
-                  IconButton(
-                      onPressed: () {}, icon: Icon(Icons.add_shopping_cart)),
-                ],
-              ),
-              Padding(
-                padding: const EdgeInsets.only(right: 16),
-                child: Text(
-                  "\$13",
-                  style: TextStyle(fontSize: 20),
+                    IconButton(
+                        onPressed: () {}, icon: Icon(Icons.add_shopping_cart)),
+                  ],
                 ),
-              ),
-            ],
-          ),
-        ],
-        title: Text("Home"),
+                Padding(
+                  padding: const EdgeInsets.only(right: 16),
+                  child: Text(
+                    "\$13",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ],
+            ),
+          ],
+          title: Text("Home"),
+        ),
       ),
     );
   }
