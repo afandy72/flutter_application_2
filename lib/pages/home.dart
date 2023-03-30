f656db0da6632b599f51bbd88be91f4cfa73615b
afandy72 <youssefafandy142@gmail.com>
2023-03-30 12:22:02 +0200

    24

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,6 +7,7 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.green,
         title: Text("data"),
       ),
     );
