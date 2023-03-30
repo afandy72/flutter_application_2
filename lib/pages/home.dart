f1d331fa2059ae637dffece5321e44b781d8bdd4
afandy72 <youssefafandy142@gmail.com>
2023-03-30 21:56:41 +0200

    47

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,8 +7,20 @@ class Item {
 }
 
 class Home extends StatelessWidget {
-  const Home({super.key});
-
+  List items = [
+    Item(imgpath: "Assets/img/-smartwatch-casio-smart-.png", price: 12.22),
+    Item(imgpath: "Assets/img/download.jpg", price: 12.22),
+    Item(imgpath: "Assets/img/png-transparent-apple-watch-l.png", price: 12.22),
+    Item(
+        imgpath:
+            "Assets/img/png-transparent-apple-watch-series-2-apple-watch-series-3-apple-watch-series-1-smart-apple-watch-sports-watch-band-watch-accessory-sport-grey-thumbnail.png",
+        price: 12.22),
+    Item(
+        imgpath:
+            "Assets/img/png-transparent-apple-watch-series-2-apple-watch-series-3-smartwatch-black-smart-watch-black-hair-digital-black-white-thumbnail.png",
+        price: 12.22),
+    Item(imgpath: "Assets/img/png-transparent-smartwatch-.png", price: 12.22)
+  ];
   @override
   Widget build(BuildContext context) {
     return SafeArea(
