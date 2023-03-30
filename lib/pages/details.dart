8ed4dbe1073d6585abc281ff4fb217e805e78409
afandy72 <youssefafandy142@gmail.com>
2023-03-30 22:51:52 +0200

    60

--- lib/pages/details.dart
+++ lib/pages/details.dart
@@ -103,6 +103,9 @@ class Details extends StatelessWidget {
                 ],
               ),
             ],
+          ),
+          SizedBox(
+            height: 16,
           )
         ],
       ),
