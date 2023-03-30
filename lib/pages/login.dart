b8854ed1c6cf5b6fca624f2fb2d84f5376912109
afandy72 <youssefafandy142@gmail.com>
2023-03-30 11:38:50 +0200

    6

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -13,38 +13,47 @@ class Login extends StatelessWidget {
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(31.0),
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            const SizedBox(
-              height: 64,
-            ),
-            MyTextField(
-              ispasss: false,
-              hinttexttt: "Enter Your E-mail: ",
-              TextInputTypeee: TextInputType.emailAddress,
-            ),
-            const SizedBox(height: 32),
-            MyTextField(
-              ispasss: true,
-              hinttexttt: "Enter Your Password: ",
-              TextInputTypeee: TextInputType.number,
-            ),
-            const SizedBox(
-              height: 64,
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text("Sign In", style: TextStyle(fontSize: 19)),
-              style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.blue),
-                  padding: MaterialStateProperty.all(EdgeInsets.all(12)),
-                  shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(6)))),
-            ),
-            const SizedBox(
-              height: 64,
-            ),
-          ]),
-        ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 64,
+              ),
+              MyTextField(
+                ispasss: false,
+                hinttexttt: "Enter Your E-mail: ",
+                TextInputTypeee: TextInputType.emailAddress,
+              ),
+              const SizedBox(height: 32),
+              MyTextField(
+                ispasss: true,
+                hinttexttt: "Enter Your Password: ",
+                TextInputTypeee: TextInputType.number,
+              ),
+              const SizedBox(
+                height: 64,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text("Sign In", style: TextStyle(fontSize: 19)),
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.blue),
+                    padding: MaterialStateProperty.all(EdgeInsets.all(12)),
+                    shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(6)))),
+              ),
+              const SizedBox(
+                height: 64,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Don't have an account??",
+                      style: TextStyle(fontSize: 17)),
+                 
+            ],
+          ),
+        ]),
       ),
     );
   }
