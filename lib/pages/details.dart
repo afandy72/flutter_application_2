cf8f2a647e28a393e67ee239f87446468a4cfd9a
afandy72 <youssefafandy142@gmail.com>
2023-03-30 23:03:50 +0200

    63

--- lib/pages/details.dart
+++ lib/pages/details.dart
@@ -39,86 +39,88 @@ class Details extends StatelessWidget {
         ],
         title: Text("Details Screen"),
       ),
-      body: Column(
-        children: [
-          Image.asset(
-              "assets/img/kisspng-apple-watch-series-2-apple-watch-series-3-smartwat-black-smart-watch-5aa3dc83def0f3.3816676915206882599132.png"),
-          SizedBox(
-            height: 16,
-          ),
-          Text("\$12.22"),
-          SizedBox(
-            height: 16,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Container(
-                padding: EdgeInsets.all(4),
-                child: Text(
-                  "New",
-                  style: TextStyle(fontSize: 16),
-                ),
-                decoration: BoxDecoration(
-                    color: Color.fromARGB(255, 173, 12, 1),
-                    borderRadius: BorderRadius.circular(4)),
-              ),
-              Row(
-                children: [
-                  Icon(
-                    Icons.star,
-                    color: Colors.yellow,
-                  ),
-                  Icon(
-                    Icons.star,
-                    color: Colors.yellow,
-                  ),
-                  Icon(
-                    Icons.star,
-                    color: Colors.yellow,
-                  ),
-                  Icon(
-                    Icons.star,
-                  ),
-                  Icon(
-                    Icons.star,
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  SizedBox(
-                    width: 80,
-                  ),
-                  Icon(
-                    Icons.edit_location,
-                  ),
-                  SizedBox(
-                    width: 3,
-                  ),
-                  Text(
-                    "Whatch Shope",
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset(
+                "assets/img/kisspng-apple-watch-series-2-apple-watch-series-3-smartwat-black-smart-watch-5aa3dc83def0f3.3816676915206882599132.png"),
+            SizedBox(
+              height: 16,
+            ),
+            Text("\$12.22"),
+            SizedBox(
+              height: 16,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  padding: EdgeInsets.all(4),
+                  child: Text(
+                    "New",
                     style: TextStyle(fontSize: 16),
-                  )
-                ],
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 40,
-          ),
-          SizedBox(
-              width: double.infinity,
-              child: Text(
-                "Details: ",
-                style: TextStyle(fontSize: 22),
-              )),
-          SizedBox(
-            height: 10,
-          ),
-          Text(
-              "Series 7 is the most durable Apple Watch ever built. Fundamental design changes were needed to achieve the vision of the larger Always-On Retina")
-        ],
+                  ),
+                  decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 173, 12, 1),
+                      borderRadius: BorderRadius.circular(4)),
+                ),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.star,
+                      color: Colors.yellow,
+                    ),
+                    Icon(
+                      Icons.star,
+                      color: Colors.yellow,
+                    ),
+                    Icon(
+                      Icons.star,
+                      color: Colors.yellow,
+                    ),
+                    Icon(
+                      Icons.star,
+                    ),
+                    Icon(
+                      Icons.star,
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    SizedBox(
+                      width: 80,
+                    ),
+                    Icon(
+                      Icons.edit_location,
+                    ),
+                    SizedBox(
+                      width: 3,
+                    ),
+                    Text(
+                      "Whatch Shope",
+                      style: TextStyle(fontSize: 16),
+                    )
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            SizedBox(
+                width: double.infinity,
+                child: Text(
+                  "Details: ",
+                  style: TextStyle(fontSize: 22),
+                )),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+                "Series 7 is the most durable Apple Watch ever built. Fundamental design changes were needed to achieve the vision of the larger Always-On Retina")
+          ],
+        ),
       ),
     );
   }
