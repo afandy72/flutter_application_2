8b1392fa8de04ac5f5fb85f94db01ea2f4a1c889
afandy72 <youssefafandy142@gmail.com>
2023-04-08 23:00:49 +0200

    78

--- lib/provider/cart.dart
+++ lib/provider/cart.dart
@@ -3,8 +3,9 @@ import 'package:flutter_application_2/model/item.dart';
 
 class cart with ChangeNotifier {
   List selected = [];
-
+  double price = 0;
   add(Item product) {
     selected.add(product);
+    notifyListeners();
   }
 }
