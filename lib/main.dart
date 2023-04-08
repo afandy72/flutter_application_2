dacd255c46daf8e2d277c91c01cb4ae3ec42f218
afandy72 <youssefafandy142@gmail.com>
2023-04-08 22:19:52 +0200

    74

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter_application_2/pages/details.dart';
 import 'package:flutter_application_2/pages/home.dart';
 import 'package:flutter_application_2/pages/login.dart';
 import 'package:flutter_application_2/pages/register.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
