e4adb0e874ad3be2a0232f33a6daafc0c747c89b
afandy72 <youssefafandy142@gmail.com>
2023-04-14 00:44:51 +0200

    100

--- lib/pages/checkout.dart
+++ lib/pages/checkout.dart
@@ -18,29 +18,52 @@ class checkout extends StatelessWidget {
         actions: [Productandprice()],
         title: Text("Check Out"),
       ),
-      body: SingleChildScrollView(
-        child: SizedBox(
-          height: 555,
-          child: ListView.builder(
-              padding: EdgeInsets.all(8),
-              itemCount: ttt.selected.length,
-              itemBuilder: (BuildContext context, int index) {
-                return Card(
-                  child: ListTile(
-                    subtitle: Text(
-                        "${ttt.selected[index].price} - ${ttt.selected[index].location}"),
-                    leading: CircleAvatar(
-                        backgroundImage:
-                            AssetImage(ttt.selected[index].imgpath)),
-                    title: Text(ttt.selected[index].name),
-                    trailing: IconButton(
-                      onPressed: () {},
-                      icon: Icon(Icons.remove),
-                    ),
-                  ),
-                );
-              }),
-        ),
+      body: Column(
+        children: [
+          SingleChildScrollView(
+            child: SizedBox(
+              height: 555,
+              child: ListView.builder(
+                  padding: EdgeInsets.all(8),
+                  itemCount: ttt.selected.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Card(
+                      child: ListTile(
+                        subtitle: Text(
+                            "${ttt.selected[index].price} - ${ttt.selected[index].location}"),
+                        leading: CircleAvatar(
+                            radius: 25,
+                            backgroundImage:
+                                AssetImage(ttt.selected[index].imgpath)),
+                        title: Text(
+                          ttt.selected[index].name,
+                          style: TextStyle(fontSize: 19),
+                        ),
+                        trailing: IconButton(
+                          onPressed: () {
+                            ttt.delete(ttt.selected[index]);
+                          },
+                          icon: Icon(Icons.remove),
+                        ),
+                      ),
+                    );
+                  }),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text(
+              "Pay ${ttt.price}",
+              style: TextStyle(fontSize: 18, color: Colors.black),
+            ),
+            style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(
+                    Color.fromARGB(255, 119, 233, 120)),
+                padding: MaterialStateProperty.all(EdgeInsets.all(12)),
+                shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8)))),
+          )
+        ],
       ),
     );
   }
