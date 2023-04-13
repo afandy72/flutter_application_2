7cfc0a9b0a8c37047132c2e66789a70f0a054704
afandy72 <youssefafandy142@gmail.com>
2023-04-13 20:49:48 +0200

    93

--- lib/pages/checkout.dart
+++ lib/pages/checkout.dart
@@ -3,6 +3,7 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:provider/provider.dart';
 
+import '../Shared/appbar.dart';
 import '../provider/cart.dart';
 
 class checkout extends StatelessWidget {
@@ -13,39 +14,7 @@ class checkout extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.black,
-        actions: [
-          Consumer<cart>(builder: ((context, www, child) {
-            return Row(
-              children: [
-                Stack(
-                  children: [
-                    Container(
-                      child: Text(
-                        "${www.selected.length}",
-                        style: TextStyle(
-                            fontSize: 17,
-                            color: Color.fromARGB(255, 4, 255, 17)),
-                      ),
-                      padding: EdgeInsets.all(5),
-                      decoration: BoxDecoration(
-                          color: Color.fromARGB(120, 255, 255, 255),
-                          shape: BoxShape.circle),
-                    ),
-                    IconButton(
-                        onPressed: () {}, icon: Icon(Icons.add_shopping_cart)),
-                  ],
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(right: 16),
-                  child: Text(
-                    "\$ ${www.price}",
-                    style: TextStyle(fontSize: 20),
-                  ),
-                ),
-              ],
-            );
-          })),
-        ],
+        actions: [Productandprice()],
         title: Text("Check Out"),
       ),
       body: SizedBox(
